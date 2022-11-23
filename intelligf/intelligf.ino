546ba38ba01298019faf73b42696818db125977c
Aditya <aditya.keerthi1@uwaterloo.ca>
2022-11-22 20:34:37 -0500

    LED switches for each mode

--- intelligf/intelligf.ino
+++ intelligf/intelligf.ino
@@ -1,20 +1,44 @@
 String inp;
+#define LED_R 10
+#define LED_C 11
+#define LED_G 12
 
 void setup() {
   Serial.begin(9600);
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(LED_R, OUTPUT);
+  pinMode(LED_C, OUTPUT);
+  pinMode(LED_G, OUTPUT);
 
 }
 
 void loop() {
   if (Serial.available() > 0) {
     inp = Serial.readStringUntil('\n');
-    if (inp == "on") {
-      digitalWrite(LED_BUILTIN, HIGH);
-      Serial.write("LED on");
-    } else if (inp == "off") {
-      digitalWrite(LED_BUILTIN, LOW);
-      Serial.write("LED off");
+    if (inp == "r") {
+      digitalWrite(LED_R, HIGH);
+      digitalWrite(LED_C, LOW);
+      digitalWrite(LED_G, LOW);
+      Serial.write("Recycling on");
+    } else if (inp == "c") {
+      digitalWrite(LED_R, LOW);
+      digitalWrite(LED_C, HIGH);
+      digitalWrite(LED_G, LOW);
+      Serial.write("Compost on");
+    } else if (inp == "g") {
+      digitalWrite(LED_R, LOW);
+      digitalWrite(LED_C, LOW);
+      digitalWrite(LED_G, HIGH);
+      Serial.write("Garbage on");
+    } else if (inp == "done") {
+      digitalWrite(LED_R, LOW);
+      digitalWrite(LED_C, LOW);
+      digitalWrite(LED_G, LOW);
+      Serial.write("All of");
+    } else if (inp == "clear") {
+      digitalWrite(LED_R, LOW);
+      digitalWrite(LED_C, LOW);
+      digitalWrite(LED_G, LOW);
+      Serial.write("Clear");
     } else {
       Serial.write("Invalid input");
     }
--- middleware.py
+++ middleware.py
@@ -1,9 +1,15 @@
 import serial
 import time
+import serial.tools.list_ports
+
+# ports = serial.tools.list_ports.comports()
+# print(ports)
 
 serialComm = serial.Serial('/dev/cu.usbmodem1101')
 serialComm.timeout = 1
-
+# r
+# c
+# g
 while True:
     inp = input("Enter input [on/off]: ").strip()
     if inp == "done":
