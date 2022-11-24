76a89baef374773ea1468f8596991e7cc949641c
Terry Su <limacechan2169@gmail.com>
2022-11-24 13:46:34 -0500

    post req

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
-from flask import Flask, request
+from flask import Flask, request,redirect
 
 app = Flask('IntelliGF')
 
 # middleware included in > middleware.py
 # use middleware to authenticate each request first
 
+curr_material = None
+
 @app.route('/', methods=['GET', 'POST'])
 def hello():
     return "Deployed on Heroku"
+
+@app.route('/edit', methods = ['POST'])
+def edit_material(material):
+    global curr_material
+    curr_material = material
+    return redirect("/")
+
+@app.route('/material', methods=['GET'])
+def hello():
+    return curr_material
+
+
+    
